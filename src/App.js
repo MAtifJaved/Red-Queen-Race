6e6bde84c93d578c0258a3b571e91f1b4d213899
MAtifJaved <atif_javed1992@yahoo.com>
2021-05-16 20:18:42 +0500

    components added

---
+++ src/App.js
@@ -0,0 +1,12 @@
+  
+import React from "react";
+import RedQueen from "./Components/RedQueen";
+function App() {
+  return (
+    <div className="App">
+      <RedQueen/>
+    </div>
+  );
+}
+
+export default App;
